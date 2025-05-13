2e7884c9195236bd6b984fbd070cd21aff2c30a7
assort_C2\assort_st2 <yuzio2326@gmail.com>
2025-05-13 12:45:44 +0900

    Monster DataTable 임시

---
+++ Source/ARPG/Monster/Pawn/MonsterBase.h
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Pawn.h"
+#include "MonsterBase.generated.h"
+USTRUCT()
+struct ARPG_API FMonsterBaseTableRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:	// Base Resource
+	UPROPERTY(EditAnywhere, Category = "Monster")
+	USkeletalMesh* SkeletalMesh = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "Monster")
+	FTransform MeshTransform = FTransform::Identity;
+
+	//Make Base Capsule
+	UPROPERTY(EditAnywhere, Category = "Monster")
+	TSubclassOf<UShapeComponent> CollisionClass = UCapsuleComponent::StaticClass();
+	UPROPERTY(EditAnywhere, Category = "Monster")
+	float CollisionCapsuleRadius = 50.f;
+	UPROPERTY(EditAnywhere, Category = "Monster")
+	float CollisionCapsuleHalfHeight = 100.f;
+
+
+public: // Animation
+	//임시 animinstance
+	UPROPERTY(EditAnywhere, Category = "Monster|Animation")
+	TSubclassOf<UAnimInstance> AnimClass;
+	UPROPERTY(EditAnywhere, Category = "Monster|Animation")
+	TArray<UAnimMontage*> HitReactMontage;
+	UPROPERTY(EditAnywhere, Category = "Monster|Animation")
+	TArray<UAnimMontage*> DieMontage;
+	// 상하체 분리 use Upper boddy
+	UPROPERTY(EditAnywhere, Category = "Monster|Animation")
+	TArray<UAnimMontage*> AttackMontage;
+public:
+	// 일반 특수 엘리트 중간보스 보스 등등		기본공격만 / 스킬사용가능 / 특수 패턴 / 기믹 등 추가하고 싶은거 추가 가능하게조절 가능
+	uint8 MonsterType = 0;
+
+
+
+	// 나중에 스킬 같은거 사용 할때 datatable 만들고 일반 몹 말고 자식에 
+	//UPROPERTY(EditAnywhere, Category = "Pawn|Skill", meta = (RowType = "/Script/ARPG.SkillTableRow"))
+	//FDataTableRowHandle OwnSkillData;
+	
+	// 나중에 아이템 드랍 같은거 할때 만들기
+	//UPROPERTY(EditAnywhere, Category = "Pawn|Drop", meta = (RowType = "/Script/ARPG.ItemTableRow"))
+	//FDataTableRowHandle OwnDropData;
+
+};
+
+UCLASS()
+class ARPG_API AMonsterBase : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMonsterBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	
+	
+};
