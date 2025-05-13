de7baef8eb18bd830144cd0ea79ee09e59cd36c3
assort_C2\assort_st2 <yuzio2326@gmail.com>
2025-05-13 18:12:37 +0900

    임시 Datatable 및 data setting 추가 몇몇 주석들로 해결 방안 추가함

--- Source/ARPG/Monster/Pawn/MonsterBase.cpp
+++ Source/ARPG/Monster/Pawn/MonsterBase.cpp
@@ -10,6 +10,51 @@ AMonsterBase::AMonsterBase()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneRoot"));
+	RootComponent = DefaultSceneRoot;
+
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
+
+	SkeletalMeshComponent->SetupAttachment(DefaultSceneRoot);
+	SkeletalMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	/*Not Use right now*/
+
+	/*if someone Use UFloatingPawnMovement Delete this Comment And Used it plz or someone do not need this delete all this codes*/
+	//MovementComponent = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("MovementComponent"));
+	//check(MovementComponent);
+
+	/*Use this Afeter Create StateComponent*/
+	//StatusComponent = CreateDefaultSubobject<UStatusComponent>(TEXT("StatusComponent"));
+	//check(StatusComponent);
+	
+	/*Use this Afeter Create SkillComponent(Finishkill) */
+	//SkillComponent = CreateDefaultSubobject<USkillComponent>(TEXT("MonsterSkillComponent"));
+	//check(SkillComponent);
+}
+
+void AMonsterBase::SetData(const FDataTableRowHandle& InDataTableRowHandle)
+{
+	MonsterDataTableRowHandle = InDataTableRowHandle;
+	if (MonsterDataTableRowHandle.IsNull()) { return; }
+	FMonsterBaseTableRow* Data = MonsterDataTableRowHandle.GetRow<FMonsterBaseTableRow>(TEXT("Enemy"));
+	if (!Data) { ensure(false); return; }
+	MonsterData = Data;
+	
+	//AI Controller Parts 
+	/*Add AI After Create*/
+	//AIControllerClass = MonsterData->AIControllerClass;
+
+	//Speed Control from Monster Data
+	/*UFloatingPawnMovement 말고 다른거 사용할 경우 MovementComponent의 해당 타입을 사용할 다른걸로 바꿔주세요*/
+	MovementComponent->MaxSpeed = MonsterData->MovementMaxSpeed;
+
+	SkeletalMeshComponent->SetSkeletalMesh(MonsterData->SkeletalMesh);
+	SkeletalMeshComponent->SetAnimClass(MonsterData->AnimClass);
+	SkeletalMeshComponent->SetRelativeTransform(MonsterData->MeshTransform);
+
+	/*Collision Component를 아직 안만들었음으로 넣지는 않음*/
+
 }
 
 // Called when the game starts or when spawned
--- Source/ARPG/Monster/Pawn/MonsterBase.h
+++ Source/ARPG/Monster/Pawn/MonsterBase.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "GameFramework/FloatingPawnMovement.h"
 #include "CoreMinimal.h"
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/Pawn.h"
@@ -38,10 +39,14 @@ public: // Animation
 	// 상하체 분리 use Upper boddy
 	UPROPERTY(EditAnywhere, Category = "Monster|Animation")
 	TArray<UAnimMontage*> AttackMontage;
-public:
+
+public: //Type & Other Datatable
 	// 일반 특수 엘리트 중간보스 보스 등등		기본공격만 / 스킬사용가능 / 특수 패턴 / 기믹 등 추가하고 싶은거 추가 가능하게조절 가능
 	uint8 MonsterType = 0;
 
+	// 타입에 따라서 Max Speed 를 조절 할수도 있어서 바꿀수 있게 함
+	UPROPERTY(EditAnywhere, Category = "Pawn|Movement")
+	float MovementMaxSpeed = 400.f;
 
 
 	// 나중에 스킬 같은거 사용 할때 datatable 만들고 일반 몹 말고 자식에 
@@ -62,6 +67,7 @@ class ARPG_API AMonsterBase : public APawn
 public:
 	// Sets default values for this pawn's properties
 	AMonsterBase();
+	virtual void SetData(const FDataTableRowHandle& InDataTableRowHandle);
 
 protected:
 	// Called when the game starts or when spawned
@@ -74,6 +80,22 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
-	
+protected:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USceneComponent> DefaultSceneRoot;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> SkeletalMeshComponent;
+
+	UPROPERTY()
+	TObjectPtr<UShapeComponent> CollisionComponent;
+
+	// load Monster Data Target
+	UPROPERTY(EditAnywhere, meta = (RowType = "/Script/ARPG.MonsterBaseTableRow"))
+	FDataTableRowHandle MonsterDataTableRowHandle;
+
+	//Make Better to Useful
+	FMonsterBaseTableRow* MonsterData;
+
+	UFloatingPawnMovement* MovementComponent;
 };
