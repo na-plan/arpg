80969dbf9323f93d78ae108398773b6f3d3f5b25
assort_C2\assort_st2 <yuzio2326@gmail.com>
2025-05-13 17:45:12 +0900

    Animinstance 기본 틀만

---
+++ Source/ARPG/Monster/DefaultAnimInstance.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "DefaultAnimInstance.generated.h"
+
+/**
+ * 
+ */
+// 현재 해당 파일 위치는 monster 입니다 추후 player 나 다른곳에 사용한다면 이동시켜 주세요
+UCLASS()
+class ARPG_API UDefaultAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	void SetAimRotation(const FRotator& NewAimRotation) { AimRotation = NewAimRotation; }
+	const FRotator& GetAimRotation() { return AimRotation; }
+
+protected:
+	UPROPERTY()
+	class UPawnMovementComponent* MovementComponent;
+
+	UPROPERTY(BlueprintReadOnly)
+	double Speed = 0.0;
+
+	UPROPERTY(BlueprintReadOnly)
+	double Direction = 0.0;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bShoudMove = false;
+
+	/*쓸지 안쓸지 고민중 + fall anim not exist */
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsCrouch = false;
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsFalling = false;
+
+	UPROPERTY(BlueprintReadOnly)
+	FRotator AimRotation;
+
+	//Playerable Character Var
+	UPROPERTY(BlueprintReadOnly)
+	bool ZoomAnim;
+	
+	
+	
+};
