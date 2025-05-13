c802f287648951278f5e9b600c957dfd0dd8e99d
assort_C2\assort_st2 <test@gmail.com>
2025-05-13 18:47:07 +0900

    05/13 상호작용 인터페이스

---
+++ Source/ARPG/Public/Interaction/NAInteractionInterface.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "NAInteractionInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI/*, meta = (CannotImplementInterfaceInBlueprint)*/)
+class UNAInteraction : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 상호작용 기능을 부여하는 인터페이스
+ */
+class ARPG_API INAInteraction
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Interaction", meta = (DisplayName = "Interact"))
+	void Interact();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Interaction", meta = (DisplayName = "Interact"))
+	bool CanInteract() const;
+};
