937057059f2c86cf311fa1edba7978da00deb540
assort_C2\assort_st2 <yuzio2326@gmail.com>
2025-05-20 12:22:03 +0900

    ﻿Gameplay Ability 추가 MonsterMontage 확인해야됌

---
+++ Source/ARPG/Private/Ability/GameAbilityTask/GATask_PlayMontageAndWaitEvent.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ability/GameAbilityTask/GATask_PlayMontageAndWaitEvent.h"
+
+
+
+
---
+++ Source/ARPG/Public/Ability/GameAbilityTask/GATask_PlayMontageAndWaitEvent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "GATask_PlayMontageAndWaitEvent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UGATask_PlayMontageAndWaitEvent : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
