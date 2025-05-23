0dbfa9c32c161c3208e687b041e3794fa97aa6e2
assort_C2\assort_st2 <yuzio2326@gmail.com>
2025-05-23 18:17:14 +0900

    DamageApplyActor 생성, GASkill 작업중 + DamageApply Target 전환시 Owner의 AbilitySystemComponent 가져오기

--- Source/ARPG/Private/Monster/Ability/GameplayAbility/GA_UseSkill.cpp
+++ Source/ARPG/Private/Monster/Ability/GameplayAbility/GA_UseSkill.cpp
@@ -3,6 +3,7 @@
 
 #include "Monster/Ability/GameplayAbility/GA_UseSkill.h"
 
-
-
-
+void UGA_UseSkill::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	
+}
