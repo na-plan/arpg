69884870c9e0dacd0e9217ca6f0984e6e9ca06d7
assort_C2\assort_st2 <yuzio2326@gmail.com>
2025-05-26 15:55:43 +0900

    Skill, Parry 만드는중

---
+++ Source/ARPG/Public/Monster/Ability/GameplayAbility/GA_ParryStun.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_ParryStun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UGA_ParryStun : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+	//Player 가 공격을 실행 하고 monster한테 전달을 해주는 방식으로 하기로함
+	//그러면? player가 공격하는 타이밍이 공격하는 몽타주의 특정 부분일때 
+public:
+
+	
+	
+	
+};
--- Source/ARPG/Public/Monster/Ability/GameplayAbility/GA_UseSkill.h
+++ Source/ARPG/Public/Monster/Ability/GameplayAbility/GA_UseSkill.h
@@ -6,6 +6,29 @@
 #include "Abilities/GameplayAbility.h"
 #include "GA_UseSkill.generated.h"
 
+
+USTRUCT()
+struct ARPG_API FNAMonsterSkillDataTableRow : public FTableRowBase
+{
+	GENERATED_BODY()
+public:
+	//사거리(원거리 근거리)
+	UPROPERTY(EditAnywhere, Category = "Skill")
+	float Distance;
+	UPROPERTY(EditAnywhere, Category = "Skill")
+	float Damage;
+	UPROPERTY(EditAnywhere, Category = "Skill")
+	float SkillCoolTime;
+	// 여러개의 몽타주 사용할 경우 차례대로 실행
+	UPROPERTY(EditAnywhere, Category = "SkillAnimation")
+	TArray<UAnimMontage*> SkillAnimation;
+
+
+
+};
+
+
+
 /**
  * 
  */
