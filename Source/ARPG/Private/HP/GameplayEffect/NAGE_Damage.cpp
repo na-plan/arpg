644f5af9639d2a0420a38f145f151d63b55eeb9e
Colibri Shin <colibrishin@gmail.com>
2025-05-29 17:57:37 +0900

    chore: remove unnecessary comment

--- Source/ARPG/Private/HP/GameplayEffect/NAGE_Damage.cpp
+++ Source/ARPG/Private/HP/GameplayEffect/NAGE_Damage.cpp
@@ -17,7 +17,4 @@ UNAGE_Damage::UNAGE_Damage()
 	DamageModifier.ModifierOp = EGameplayModOp::Additive;
 	DamageModifier.ModifierMagnitude = FScalableFloat(-10.f);
 	Modifiers.Add(DamageModifier);
-
-	UE_LOG(LogGameplayEffects, Log, TEXT( "[%hs]: Damage %s will try to be applied" ), __FUNCTION__, *DamageModifier.ModifierMagnitude.GetValueForEditorDisplay().ToString() );
-	
 }
