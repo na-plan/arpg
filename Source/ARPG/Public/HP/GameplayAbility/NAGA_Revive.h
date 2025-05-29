055510012854afb7a6e14f60ed2b3cac99eec773
Colibri Shin <colibrishin@gmail.com>
2025-05-29 17:57:50 +0900

    todo/feat: reviving other player

---
+++ Source/ARPG/Public/HP/GameplayAbility/NAGA_Revive.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "NAGA_Revive.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UNAGA_Revive : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	virtual void CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateCancelAbility) override;
+
+	virtual void CommitExecute(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+
+	
+};
