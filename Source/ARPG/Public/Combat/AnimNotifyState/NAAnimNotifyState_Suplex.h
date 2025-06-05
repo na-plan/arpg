5e4c35fb7af1fba7eddf1be18814cb1e5ef6d14c
assort_C2\assort_st2 <yuzio2326@gmail.com>
2025-06-05 13:12:07 +0900

    rebasing2

--- Source/ARPG/Public/Combat/AnimNotifyState/NAAnimNotifyState_Suplex.h
+++ Source/ARPG/Public/Combat/AnimNotifyState/NAAnimNotifyState_Suplex.h
@@ -14,7 +14,45 @@ class ARPG_API UNAAnimNotifyState_Suplex : public UAnimNotifyState
 {
 	GENERATED_BODY()
 	
-	
+
+	// 시작
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	// 끝
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+	// 확인
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+
+	bool SuccessSuplex = false;
+public:
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Overlap", meta = (AllowPrivateAccess = "true"))
+	//float OverlapInterval = 0.01f;
+	//float OverlapElapsed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Overlap", meta = (AllowPrivateAccess = "true"))
+	FName SocketName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Overlap", meta = (AllowPrivateAccess = "true"))
+	float SphereRadius = 20.f;
+
+	//당하는 montage
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Montage", meta = (AllowPrivateAccess = "true"))
+	UAnimMontage* SuplexedMontage;
+
+	//시전하는 montage
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Montage", meta = (AllowPrivateAccess = "true"))
+	UAnimMontage* SuplexMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sound", meta = (AllowPrivateAccess = "true"))
+	USoundBase* SuplexSound = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Montage", meta = (AllowPrivateAccess = "true"))
+	float Damage = 100;
+
+	UPROPERTY()
+	TSet<AActor*> AppliedActors;
+
+	FGameplayEffectContextHandle ContextHandle;
+
+	FGameplayEffectSpecHandle SpecHandle;
 	
 	
 };
