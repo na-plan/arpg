5eebbb19881102bd90f732f237176d55af369742
Colibri Shin <colibrishin@gmail.com>
2025-06-08 17:24:00 +0900

    fix: revive widget destroyed

--- Source/ARPG/Private/Interaction/NAInteractionComponent.cpp
+++ Source/ARPG/Private/Interaction/NAInteractionComponent.cpp
@@ -195,18 +195,18 @@ void UNAInteractionComponent::UpdateInteractionData()
 	float MinDistance = TNumericLimits<float>::Max();
 	TWeakInterfacePtr<INAInteractableInterface> BestInteractable;
 
-	for (auto& It : FocusedInteractableMap)
+	for ( auto It = FocusedInteractableMap.CreateIterator(); It; ++It )
 	{
-		if (!It.Key.IsValid())
+		if (!It->Key.IsValid())
 		{
 			continue;
 		}
-		if (It.Key.GetRawObject() != It.Value.FocusedInteractable.Get())
+		if (It->Key.GetRawObject() != It->Value.FocusedInteractable.Get())
 		{
 			continue;
 		}
 
-		It.Value.LastInteractionCheckTime = GetWorld()->GetTimeSeconds();
+		It->Value.LastInteractionCheckTime = GetWorld()->GetTimeSeconds();
 		FVector OwnerLoc;
 		if (APawn* OwningPawn = Cast<APawn>(GetOwner()))
 		{
@@ -225,14 +225,20 @@ void UNAInteractionComponent::UpdateInteractionData()
 			}
 		}
 		
-		FVector InteractableLoc = It.Value.FocusedInteractable->GetActorLocation();
-		It.Value.DistanceToActiveInteractable = FVector::Dist(OwnerLoc, InteractableLoc);
+		FVector InteractableLoc = It->Value.FocusedInteractable->GetActorLocation();
+		It->Value.DistanceToActiveInteractable = FVector::Dist(OwnerLoc, InteractableLoc);
+
+		if ( It->Value.FocusedInteractable->GetAttachParentActor() )
+		{
+			It.RemoveCurrent();
+			continue;
+		}
 
 		// 최소 거리 인터랙터블 검사
-		if (It.Value.DistanceToActiveInteractable < MinDistance)
+		if (It->Value.DistanceToActiveInteractable < MinDistance)
 		{
-			MinDistance       = It.Value.DistanceToActiveInteractable;
-			BestInteractable  = It.Key.ToWeakInterface();
+			MinDistance       = It->Value.DistanceToActiveInteractable;
+			BestInteractable  = It->Key.ToWeakInterface();
 		}
 	}
 
@@ -291,6 +297,12 @@ bool UNAInteractionComponent::OnInteractableFound(TScriptInterface<INAInteractab
 		FNAInteractionData NewInteractionData;
 		NewInteractionData.FocusedInteractable = InteractableItem;
 
+		if ( InteractableItem->GetAttachParentActor() )
+		{
+			// UE_LOG( LogTemp, Log, TEXT("%hs: %s 아이템 주인이 이미 있음"), __FUNCTION__, *GetNameSafe( InteractableItem ) )
+			return false;
+		}
+
 		FocusedInteractableMap.Emplace(InteractableActor, NewInteractionData);
 
 		if (!IsComponentTickEnabled())
@@ -317,7 +329,7 @@ bool UNAInteractionComponent::OnInteractableLost(TScriptInterface<INAInteractabl
 
 	if (FocusedInteractableMap.IsEmpty())
 	{
-		UE_LOG(LogTemp, Warning, TEXT("[UNAInteractionComponent::OnInteractableLost]  FocusedInteractableMap이 비어있었는데??"));
+		// UE_LOG(LogTemp, Warning, TEXT("[UNAInteractionComponent::OnInteractableLost]  FocusedInteractableMap이 비어있었는데??"));
 		return false;
 	}
 
